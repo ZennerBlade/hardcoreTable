a1d9aca692cd21401c9f55d09b5fa1e61e10267b
Prateek <coolprateek.kumar@gmail.com>
2022-09-03 16:54:15 +0530

    Removed comments

--- src/table/column.tsx
+++ src/table/column.tsx
@@ -1,55 +1,6 @@
 import { Person } from './data';
-import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { createColumnHelper } from '@tanstack/react-table';
 import Checkbox from './Checkbox';
-
-// export const columns: ColumnDef<Person>[] = [
-//   {
-//     id: 'select',
-//     header: ({ table }) => (
-//       <Checkbox
-//         {...{
-//           checked: table.getIsAllRowsSelected(),
-//           indeterminate: table.getIsSomeRowsSelected(),
-//           onChange: table.getToggleAllRowsSelectedHandler(),
-//         }}
-//       />
-//     ),
-// cell: ({ row }) => (
-//   <Checkbox
-//     {...{
-//       checked: row.getIsSelected(),
-//       indeterminate: row.getIsSomeSelected(),
-//       onChange: row.getToggleSelectedHandler(),
-//     }}
-//   />
-// ),
-//   },
-//   {
-//     id: 'firstName',
-//     header: 'firstName',
-//   },
-//   {
-//     id: 'lastName',
-//     header: () => <span>Last Name</span>,
-//     cell: (info) => <i>{info.getValue<string | number>()}</i>,
-//   },
-//   {
-//     id: 'age',
-//     header: 'Age',
-//   },
-//   {
-//     id: 'visits',
-//     header: () => <span>Visits</span>,
-//   },
-//   {
-//     id: 'status',
-//     header: 'Status',
-//   },
-//   {
-//     id: 'progress',
-//     header: 'Profile Progress',
-//   },
-// ];
 
 const columnHelper = createColumnHelper<Person>();
 
@@ -94,13 +45,3 @@ export const columns = [
     header: 'Profile Progress',
   }),
 ];
-
-{
-  /* <Checkbox
-        {...{
-          checked: table.getIsAllRowsSelected(),
-          indeterminate: table.getIsSomeRowsSelected(),
-          onChange: table.getToggleAllRowsSelectedHandler(),
-        }}
-      /> */
-}
